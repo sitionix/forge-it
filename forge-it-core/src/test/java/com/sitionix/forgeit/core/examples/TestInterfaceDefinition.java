2735439a4529e921df0ef674bc5b873429725dc6
vladvinskevitch <vvinskevitch@gmail.com>
2025-11-08 06:58:47 +0200

    feat: update java version

--- forge-it-core/src/test/java/com/sitionix/forgeit/core/examples/TestInterfaceDefinition.java
+++
@@ -1,9 +0,0 @@
-package com.sitionix.forgeit.core.examples;
-
-import com.sitionix.forgeit.core.annotation.ForgeFeatures;
-import com.sitionix.forgeit.core.api.ForgeIT;
-import com.sitionix.forgeit.wiremock.api.WireMockSupport;
-
-@ForgeFeatures(WireMockSupport.class)
-public interface TestInterfaceDefinition extends ForgeIT {
-}
