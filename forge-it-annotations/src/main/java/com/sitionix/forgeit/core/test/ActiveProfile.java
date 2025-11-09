981ead30439c2a9608a462afed628ae0bfaeeb5f
sitionix <sitionixvv@gmail.com>
2025-11-09 13:42:14 +0200

    Add IT profile and MockMvc support to IntegrationTest

---
+++ forge-it-annotations/src/main/java/com/sitionix/forgeit/core/test/ActiveProfile.java
@@ -0,0 +1,12 @@
+package com.sitionix.forgeit.core.test;
+
+/**
+ * Shared Spring profile constants for ForgeIT test contexts.
+ */
+public final class ActiveProfile {
+
+    public static final String IT = "it";
+
+    private ActiveProfile() {
+    }
+}
