4b22df91d24b28029ef70f153f7490c3d2445129
sitionix <sitionixvv@gmail.com>
2025-11-19 07:13:25 +0200

    Remove comment from test application

--- forge-it-core/src/test/java/com/sitionix/forgeit/core/testing/TestApplication.java
+++ forge-it-core/src/test/java/com/sitionix/forgeit/core/testing/TestApplication.java
@@ -2,11 +2,6 @@ package com.sitionix.forgeit.core.testing;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Minimal Spring Boot configuration used to bootstrap the test application context.
- * The class lives in the test scope only and acts as the anchor for component scanning
- * during integration tests.
- */
 @SpringBootApplication
 public class TestApplication {
 }
