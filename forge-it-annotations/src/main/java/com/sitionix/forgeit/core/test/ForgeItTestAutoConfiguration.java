b004474cb358cb7dab3d12ce507b88ec86d829b4
sitionix <sitionixvv@gmail.com>
2025-11-29 10:33:44 +0200

    Ensure application beans are available for integration tests

--- forge-it-annotations/src/main/java/com/sitionix/forgeit/core/test/ForgeItTestAutoConfiguration.java
+++ forge-it-annotations/src/main/java/com/sitionix/forgeit/core/test/ForgeItTestAutoConfiguration.java
@@ -1,7 +1,11 @@
 package com.sitionix.forgeit.core.test;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
+@ConditionalOnClass(name = "com.sitionix.forgeit.application.loader.ResourcesLoaderImpl")
+@ComponentScan(basePackages = "com.sitionix.forgeit.application")
 public class ForgeItTestAutoConfiguration {
 }
