10f283fd3199334f8db48d83231dbd66681c27e5
vladvinskevitch <vvinskevitch@gmail.com>
2025-12-12 13:40:57 +0200

    [SITIONIX-24] - add basics for postgresql support

--- forge-it-annotations/src/main/java/com/sitionix/forgeit/core/test/IntegrationTest.java
+++ forge-it-annotations/src/main/java/com/sitionix/forgeit/core/test/IntegrationTest.java
@@ -1,8 +1,13 @@
 package com.sitionix.forgeit.core.test;
 
+import com.sitionix.forgeit.application.executor.ForgeItDbCleanupTestExecutionListener;
+import com.sitionix.forgeit.core.contract.DbCleanup;
 import com.sitionix.forgeit.domain.contract.clean.CleanupPhase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.core.annotation.AliasFor;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
@@ -14,13 +19,19 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ForgeItTest
-@ForgeItDatabaseTest
+@Transactional(transactionManager = "transactionManager")
+@Rollback
+@TestExecutionListeners(
+        listeners = ForgeItDbCleanupTestExecutionListener.class,
+        mergeMode = TestExecutionListeners.MergeMode.MERGE_WITH_DEFAULTS
+)
+@DbCleanup(phase = CleanupPhase.NONE)
 @AutoConfigureMockMvc
 public @interface IntegrationTest {
 
-    @AliasFor(annotation = ForgeItDatabaseTest.class, attribute = "rollback")
+    @AliasFor(annotation = Rollback.class, attribute = "value")
     boolean rollback() default true;
 
-    @AliasFor(annotation = ForgeItDatabaseTest.class, attribute = "cleanupPhase")
+    @AliasFor(annotation = DbCleanup.class, attribute = "phase")
     CleanupPhase cleanupPhase() default CleanupPhase.NONE;
 }
